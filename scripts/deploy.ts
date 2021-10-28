ff89bd54b34a2f72946cfc1b612fc70d743be05d
Brian Li <zhenbangli@gmail.com>
2021-10-28 18:25:47 -0500

    chore: full anonymice deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import traitTypes from "../data/trait_types.json";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -15,11 +16,36 @@ async function main() {
 
   // We get the contract to deploy
   const Anonymice = await ethers.getContractFactory("Anonymice");
-  const contract = await Anonymice.deploy();
+  const anonymiceContract = await Anonymice.deploy();
 
-  await contract.deployed();
+  await anonymiceContract.deployed();
 
-  console.log("contract deployed to:", contract.address);
+  console.log("anonymice contract deployed to:", anonymiceContract.address);
+
+  const Cheeth = await ethers.getContractFactory("Cheeth");
+  const cheethContract = await Cheeth.deploy();
+
+  await cheethContract.deployed();
+
+  console.log("cheeth contract deployed to:", cheethContract.address);
+
+  await anonymiceContract.setCheethAddress(cheethContract.address);
+
+  console.log("anonymice cheeth address set");
+
+  await cheethContract.setAnonymiceAddress(anonymiceContract.address);
+
+  console.log("cheeth anonymice address set");
+
+  for (let { _traitTypeIndex, traits } of traitTypes) {
+    await anonymiceContract.addTraitType(_traitTypeIndex, traits);
+    console.log(
+      "anonymice trait types added:",
+      _traitTypeIndex,
+      traits[0].traitType,
+      traits.map((t) => t.traitName)
+    );
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
