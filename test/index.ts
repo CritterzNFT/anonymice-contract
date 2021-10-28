6cc717d666725b26308520a6f2311eb754cf8ddd
Brian Li <zhenbangli@gmail.com>
2021-10-27 20:09:04 -0500

    add more test case

--- test/index.ts
+++ test/index.ts
@@ -2,8 +2,9 @@ import { BigNumber } from "@ethersproject/bignumber";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import traitTypes from "../data/trait_types.json";
+import { Anonymice } from "../typechain";
 
-async function setup() {
+const setup = async () => {
   const Anonymice = await ethers.getContractFactory("Anonymice");
   const contract = await Anonymice.deploy();
   await contract.deployed();
@@ -11,9 +12,16 @@ async function setup() {
     await contract.addTraitType(_traitTypeIndex, traits);
   }
   return contract;
-}
+};
 
-describe("AddTraitType", function () {
+const mintN = async (contract: Anonymice, n: number) => {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+    await contract.mintMouse();
+  }
+};
+
+describe("AddTraitType", () => {
   it("Trait types should match added trait types.", async function () {
     const contract = await setup();
     for (let i in traitTypes) {
@@ -33,5 +41,24 @@ describe("AddTraitType", function () {
         ]);
       }
     }
+  });
+});
+
+describe("mintMouse", () => {
+  it("Should mint a mouse.", async function () {
+    const contract = await setup();
+    await mintN(contract, 1);
+    expect(await contract.totalSupply()).to.equal(1);
+    await mintN(contract, 35);
+    expect(await contract.totalSupply()).to.equal(36);
+  });
+});
+
+describe("currentCheethCost", () => {
+  it("Should return the correct cost", async () => {
+    const contract = await setup();
+    expect(await contract.currentCheethCost(), "incorrect cost for 0").to.equal(
+      BigNumber.from(0)
+    );
   });
 });
